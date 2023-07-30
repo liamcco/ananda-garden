ce944ee6d3a344bbcd6ed88e4f2cbdeb43faa460
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-07-30 13:16:52 +0200

    added hostelinfo

--- src/app/vandrarhem/page.tsx
+++ src/app/vandrarhem/page.tsx
@@ -1,11 +1,13 @@
-import { BookNow, NewsPreview } from "@/containers";
+import { BookNow, NewsPreview, Article } from "@/containers";
 import PageHeader from "@/containers/PageHeader";
+import { hostelInfo } from "@/mockdata/info";
 
 function Vandrarhem() {
   return (
     <div className="flex flex-col gap-8">
       <PageHeader title="Vandrarhem" />
       <BookNow />
+      <Article content={hostelInfo} />
       <NewsPreview />
     </div>
   );
