a3f48ad968a683d38baadc3d99db06a4695476cc
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-08-15 17:18:36 +0200

    small switcharoo

--- src/containers/hostel/HostelOverview.tsx
+++ src/containers/hostel/HostelOverview.tsx
@@ -4,8 +4,16 @@ import { lakshmi, ganesha, hanuman } from "@/mockdata/images";
 export default function HostelOverview() {
   return (
     <div className="container">
+      <div className="flex justify-center mb-8">
+        <a
+          href="/vandrarhem/prices"
+          className="py-2 px-8 bg-primary-400 rounded-lg center"
+        >
+          Priser & Bokning
+        </a>
+      </div>
       <h2 className="text-3xl font-bold text-center">Våra rum</h2>
-      <div className="flex flex-wrap mb-8 justify-around">
+      <div className="flex flex-wrap justify-around">
         <a href="/vandrarhem/room/lakshmi">
           <Ball name="Lakshmi" image={lakshmi.src} />
         </a>
@@ -14,14 +22,6 @@ export default function HostelOverview() {
         </a>
         <a href="/vandrarhem/room/hanuman">
           <Ball name="Hauman" image={hanuman.src} />
-        </a>
-      </div>
-      <div className="flex justify-center">
-        <a
-          href="/vandrarhem/prices"
-          className="py-2 px-8 bg-primary-400 rounded-lg center"
-        >
-          Priser & Bokning
         </a>
       </div>
     </div>
